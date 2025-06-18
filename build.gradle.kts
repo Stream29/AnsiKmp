9e49534feb74224f22cd77d53b6e22789e89c985
Stream <1542763342@qq.com>
2025-06-19 02:02:09 +0800

    build: build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,16 @@
+@file:OptIn(ExperimentalWasmDsl::class, ExperimentalKotlinGradlePluginApi::class)
+
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JsModuleKind
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithTests
+import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
-    kotlin("jvm") version "2.1.20"
+    kotlin("multiplatform") version "2.1.20"
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 group = "io.github.stream29"
@@ -9,13 +20,98 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
     jvmToolchain(21)
+    explicitApi()
+    jvm()
+    jvmToolchain(8)
+
+    js {
+        nodejs {
+            testTask {
+                useMocha {
+                    timeout = "10s"
+                }
+            }
+        }
+
+        compilerOptions {
+            sourceMap = true
+            moduleKind = JsModuleKind.MODULE_UMD
+        }
+    }
+
+    wasmJs {
+        nodejs()
+    }
+    wasmWasi {
+        nodejs()
+    }
+
+    linuxArm64()
+    macosX64()
+    macosArm64()
+    iosSimulatorArm64()
+    iosX64()
+
+    // Tier 2
+    linuxX64()
+    linuxArm64()
+    watchosSimulatorArm64()
+    watchosX64()
+    watchosArm32()
+    watchosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    tvosArm64()
+    iosArm64()
+
+    // Tier 3
+    mingwX64()
+    watchosDeviceArm64()
+    androidNativeArm32()
+    androidNativeArm64()
+    androidNativeX86()
+    androidNativeX64()
+
+    targets.withType<KotlinNativeTarget>().configureEach {
+        binaries.test(listOf(NativeBuildType.RELEASE))
+    }
+    targets.withType<KotlinNativeTargetWithTests<*>>().configureEach {
+        testRuns.create("releaseTest") {
+            setExecutionSourceFrom(binaries.getTest(NativeBuildType.RELEASE))
+        }
+    }
+    applyDefaultHierarchyTemplate {}
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+    coordinates((group as String), "ansi-kmp", version.toString())
+    pom {
+        name.set("AnsiKmp")
+        description.set("ANSI in KMP.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/Stream29/AnsiKmp")
+        licenses {
+            license {
+                name.set("Apache License Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                distribution.set("https://www.apache.org/licenses/LICENSE-2.0")
+            }
+        }
+        developers {
+            developer {
+                id.set("Stream29")
+                name.set("Stream")
+                url.set("https://github.com/Stream29/")
+            }
+        }
+        scm {
+            url.set("https://github.com/Stream29/AnsiKmp")
+            connection.set("scm:git:git://github.com/Stream29/AnsiKmp.git")
+            developerConnection.set("scm:git:ssh://git@github.com:Stream29/AnsiKmp.git")
+        }
+    }
 }
